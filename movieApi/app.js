55eeb758df629b1afb02687504b2910c52f8d788
Karthik <jskarthik1@gmail.com>
2021-06-26 19:15:39 +0530

    Completed movieFanSite Project

--- movieApi/app.js
+++ movieApi/app.js
@@ -3,19 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet')
 
 var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
-const helmet = require('helmet')
-// app.use(helmet())
-// app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", 'maxcdn.bootstrapcdn.com', 'ajax.googleapis.com'], styleSrc: ["'self'", 'maxcdn.bootstrapcdn.com'], imgSrc: ["'self'", 'image.tmdb.org'] } }))
-
+app.use(helmet())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- movieFanSite/routes/index.js
+++ movieFanSite/routes/index.js
@@ -9,6 +9,8 @@ const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
 const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
 router.use((req, res, next) => {
+  // Added imageBaseUrl to res.locals so that EVERY ROUTE 
+  // HAS access to imageBaseUrl
   res.locals.imageBaseUrl = imageBaseUrl;
   next();
 })
@@ -18,33 +20,79 @@ router.use((req, res, next) => {
 router.get('/', function (req, res, next) {
   
   // Before we render, we want to make a GET request to an API
-  // to get the information
+  // to get the information. That is done using request.get()
 
   // request.get takes 2 args:
   // 1. it takes the URL to http "GET"
-  // 2. the callback to run when the http resopnse is back. (This callback takes 3 args)
+  // 2. the callback to run when the http response is received. (This callback takes 3 args)
   //   1. error (if any)
   //   2. http response by the API Server
   //   3. json/data the server sent back
   request.get(nowPlayingUrl, (error, response, movieData) => {
     // console.log("===================== The Error =====================")
-    // console.log(error);
+    // console.log(error); // If there is no error, this will be 'null'
     // console.log("===================== The Response =====================")
     // console.log(response);
     // The actual data is present on the body property of the response.
     // It is in string format and needs to be parsed as JSON.
     const parsedData = JSON.parse(movieData);
-    // movieData = response.body Both are same!
-    console.log(movieData);
+    // movieData = response.body Both are same! It is a massive string.
+    // data over HTTP messages is sent as strings. Thats how HTTP works.
+    // console.log(movieData);
     // console.log("===================== The Type (string) =====================")
     // console.log(typeof movieData)
     // console.log(typeof response.body)
 
     
     // res.json(parsedData)
-    res.render('index', {parsedData: parsedData.results});
+    res.render('index', { parsedData: parsedData.results });
+    // With This... parsedData will be added to res.locals for use in template index.
+    // This is valid for '/' route ONLY! remember that!
   })
 });
+
+// /movie/:id is a wildcard route.
+// that means :id is goind to be stored in... req.params.id
+router.get('/movie/:id', (req, res, next) => {
+  // res.json(req.params.id);
+  const movieId = req.params.id;
+  const thisMovieUrl = `${apiBaseUrl}/movie/${movieId}?api_key=${apiKey}`;
+  // res.send(thisMovieUrl)
+  request.get(thisMovieUrl, (error, response, movieData) => {
+    const parsedData = JSON.parse(movieData);
+    res.render('single-movie', { parsedData })
+    // This is ES6 Way of passing parsedData
+  })
+})
+
+router.post("/search", (req, res, next) => {
+  // res.send("Sanity Check")
+  const userSearchTerm = encodeURI(req.body.movieSearch);
+  // The value typed in the input element has name = "movieSearch"
+  // When data is sent to server... it will be stored inside req.body.movieSearch.
+  // This is how it works...
+
+  const cat = req.body.cat
+  // Chosen category will be sent to server inside req.body.cat
+  // This can be "movie" or "person"
+
+  const movieUrl = `${apiBaseUrl}/search/${cat}?query=${userSearchTerm}&api_key=${apiKey}`
+  // res.send(movieUrl)
+  request.get(movieUrl, (error, response, movieData) => {
+    let parsedData = JSON.parse(movieData)
+    // res.json(parsedData)
+
+    if (cat === "person") {
+      parsedData.results = parsedData.results[0].known_for;
+    }
+    res.render('index', {
+      parsedData: parsedData.results
+    })
+  })
+
+
+
+})
 
 module.exports = router;
 // 86e55bf0fa416fe8d7e8052b89be38ad
